df6f1f300f488cdc0823748919f758041ecf1dce
cinder block games <hello@cinderblockgames.com>
2022-05-09 17:50:19 -0400

    Created quick app to fill database addresses.

--- WaxRentals/FillDatabaseAddresses/Program.cs
+++ WaxRentals/FillDatabaseAddresses/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Data.SqlClient;
+using Nano.Net;
 
 namespace FillDatabaseAddresses
 {
@@ -6,7 +8,37 @@ namespace FillDatabaseAddresses
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (var connection = new SqlConnection(args[0]))
+            {
+                connection.Open();
+
+                uint max = 0;
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = "SELECT MAX(AddressId) FROM Address";
+                    var result = command.ExecuteScalar();
+                    if (!(result is DBNull))
+                    {
+                        max = (uint)result;
+                    }
+                }
+
+                for (uint outer = 0; outer < 1000; outer++)
+                {
+                    using (var command = connection.CreateCommand())
+                    {
+                        command.CommandText = "INSERT INTO Address (AddressId, Address) VALUES ";
+                        for (uint inner = 1; inner <= 1000; inner++)
+                        {
+                            var id = max + (1000 * outer) + inner;
+                            var account = new Account(args[1], id, "ban");
+                            command.CommandText += $"({id}, '{account.Address}'), ";
+                        }
+                        command.CommandText = command.CommandText.TrimEnd(' ', ',');
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
         }
     }
 }
