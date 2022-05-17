ab01470b35e820f0aec0c74a3db994087711d488
cinder block games <hello@cinderblockgames.com>
2022-05-16 21:02:25 -0400

    Keeping track of branano spend.

--- WaxRentals/WaxRentals.Processing/Processors/PurchaseProcessor.cs
+++ WaxRentals/WaxRentals.Processing/Processors/PurchaseProcessor.cs
@@ -2,7 +2,10 @@
 using System.Threading.Tasks;
 using WaxRentals.Data.Entities;
 using WaxRentals.Data.Manager;
+using WaxRentals.Monitoring.Logging;
+using WaxRentals.Monitoring.Prices;
 using WaxRentals.Waxp.Transact;
+using static WaxRentals.Monitoring.Config.Constants;
 using BananoAccount = WaxRentals.Banano.Transact.IBananoAccount;
 
 namespace WaxRentals.Processing.Processors
@@ -12,20 +15,24 @@ namespace WaxRentals.Processing.Processors
 
         private BananoAccount Banano { get; }
         private IWaxAccounts Wax { get; }
+        private IPriceMonitor Prices { get; }
 
-        public PurchaseProcessor(IProcess data, ILog log, BananoAccount banano, IWaxAccounts wax)
+        public PurchaseProcessor(IProcess data, ILog log, BananoAccount banano, IWaxAccounts wax, IPriceMonitor prices)
             : base(data, log)
         {
             Banano = banano;
             Wax = wax;
+            Prices = prices;
         }
 
         protected override Func<Task<Purchase>> Get => Data.PullNextPurchase;
         protected override async Task Process(Purchase purchase)
         {
             var hash = await Banano.Send(purchase.PaymentBananoAddress, purchase.Banano);
-            await Data.ProcessPurchase(purchase.PurchaseId, hash);
-            await Wax.Primary.Send(Wax.Today.Account, purchase.Wax);
+            var dataTask = Data.ProcessPurchase(purchase.PurchaseId, hash);
+            var waxTask = Wax.Primary.Send(Wax.Today.Account, purchase.Wax);
+            Tracker.Track("Sent BAN", purchase.Banano, Coins.Banano, spent: purchase.Banano * Prices.Banano);
+            await Task.WhenAll(dataTask, waxTask);
         }
 
     }
