1afecc6738db2e860c23ed84d22a80f18c3c181d
cinder block games <hello@cinderblockgames.com>
2022-05-18 15:22:58 -0400

    Finished SELL WAX tab.

--- WaxRentals/WaxRentalsWeb/Data/AppStateModel.cs
+++ WaxRentals/WaxRentalsWeb/Data/AppStateModel.cs
@@ -20,18 +20,18 @@
         public AppStateModel(AppState state)
         {
             BananoBalance        = decimal.Round(state.BananoBalance.Value      , 4);
-            WaxBalanceAvailable  = decimal.Round(state.WaxBalanceAvailable.Value, 4);
-            WaxBalanceStaked     = decimal.Round(state.WaxBalanceStaked.Value   , 4);
+            WaxBalanceAvailable  = decimal.Round(state.WaxBalanceAvailable.Value, 0);
+            WaxBalanceStaked     = decimal.Round(state.WaxBalanceStaked.Value   , 0);
             WaxBalanceUnstaking  = decimal.Round(state.WaxBalanceUnstaking.Value, 4);
             BananoPrice          = decimal.Round(state.BananoPrice.Value        , 4);
             WaxPrice             = decimal.Round(state.WaxPrice.Value           , 4);
             WaxRentPriceInBanano = decimal.Round(state.WaxRentPriceInBanano     , 4);
             WaxBuyPriceInBanano  = decimal.Round(state.WaxBuyPriceInBanano      , 4);
             BananoMinimumCredit  = decimal.Round(state.BananoMinimumCredit      , 4);
-            WaxMinimumRent       = decimal.Round(state.WaxMinimumRent           , 4);
-            WaxMaximumRent       = decimal.Round(state.WaxMaximumRent           , 4);
-            WaxMinimumBuy        = decimal.Round(state.WaxMinimumBuy            , 4);
-            WaxMaximumBuy        = decimal.Round(state.WaxMaximumBuy            , 4);
+            WaxMinimumRent       = decimal.Round(state.WaxMinimumRent           , 0);
+            WaxMaximumRent       = decimal.Round(state.WaxMaximumRent           , 0);
+            WaxMinimumBuy        = decimal.Round(state.WaxMinimumBuy            , 0);
+            WaxMaximumBuy        = decimal.Round(state.WaxMaximumBuy            , 0);
         }
 
     }
